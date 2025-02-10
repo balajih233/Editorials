49d2c3a6862299464b562fcbdf5475a2e5430627
Balaji <balaji718b@gmail.com>
2025-02-10 22:12:20 +0530

    New editorial added

---
+++ cf.js
@@ -0,0 +1,7 @@
+function showCode(language) {
+    
+    document.getElementById('saa-java').style.display = (language === 'java') ? 'block' : 'none';
+    document.getElementById('saa-py').style.display = (language === 'python') ? 'block' : 'none';
+    document.getElementById('sao-java').style.display = (language === 'java') ? 'block' : 'none';
+    document.getElementById('sao-py').style.display = (language === 'python') ? 'block' : 'none';
+}
--- lc.js
+++ lc.js
@@ -1,6 +1,6 @@
 function showCode(language) {
     document.getElementById('java-code').style.display = (language === 'java') ? 'block' : 'none';
     document.getElementById('python-code').style.display = (language === 'python') ? 'block' : 'none';
-    document.getElementById('LT-java').style.display = (language === 'java') ? 'block' : 'none';
-    document.getElementById('LT-py').style.display = (language === 'python') ? 'block' : 'none';
+    document.getElementById('smd-java').style.display = (language === 'java') ? 'block' : 'none';
+    document.getElementById('smd-py').style.display = (language === 'python') ? 'block' : 'none';
 }
