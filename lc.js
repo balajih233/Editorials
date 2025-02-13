0b5b019f9c71097e4040daab57aba8710f6120f2
Balaji <balaji718b@gmail.com>
2025-02-13 18:17:12 +0530

    Palindrome number editorial added

--- lc.js
+++ lc.js
@@ -3,4 +3,8 @@ function showCode(language) {
     document.getElementById('python-code').style.display = (language === 'python') ? 'block' : 'none';
     document.getElementById('smd-java').style.display = (language === 'java') ? 'block' : 'none';
     document.getElementById('smd-py').style.display = (language === 'python') ? 'block' : 'none';
+    document.getElementById('pn-java').style.display = (language === 'java') ? 'block' : 'none';
+    document.getElementById('pn-py').style.display = (language === 'python') ? 'block' : 'none';
+    document.getElementById('pn1-java').style.display = (language === 'java') ? 'block' : 'none';
+    document.getElementById('pn1-py').style.display = (language === 'python') ? 'block' : 'none';
 }
